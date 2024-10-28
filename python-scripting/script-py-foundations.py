b33b87c33d9ca4e8dfe65eebc6ad8c713bca74f9
AG-Samaniego <asama6@illinois.edu>
2024-10-28 16:21:21 -0500

    python scripting foundations started

---
+++ python-scripting/script-py-foundations.py
@@ -0,0 +1,20 @@
+favorite_movies =[
+    {
+        "name": "The Matrix I",
+        "release_year": 1999,
+        "sequels": ["The Matrix II", "The Matrix III", "The Matrix IV"]
+    },
+    {
+        "name": "Star Wars IV",
+        "release_year": 1977,
+        "sequels": ["Star Wars V", "Star Wars VI", "Star Wars VII", "Star Wars VIII", "Star Wars IX"],
+        "prequels": ["Star Wars I", "Star Wars II", "Star Wars III"]
+    }
+]
+# Outline: Take movie as argument
+# Check if movie released < 2000 --> Print "This movie was released before 2000"
+# ONLY RETURN IF RELEASED AFTER 2000
+def check_movie_year(movie):
+    if movie
+
+# Below function, create empty list recent_movies
